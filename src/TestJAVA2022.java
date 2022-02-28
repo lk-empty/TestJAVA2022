f845d671babc068b39856b4ee2f9d9824b862090
lk-empty <39465620+lk-empty@users.noreply.github.com>
2022-02-28 14:53:22 +0800

    2022testcommit1

---
+++ src/TestJAVA2022.java
@@ -0,0 +1,19 @@
+public class TestJAVA2022 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("2021.9.5");
+        System.out.println("2021.9.19");
+        System.out.println("2021.9.19");
+        System.out.println("2021.9.19");
+        System.out.println("2021.9.19");
+        System.out.println("2021.10.29");
+        System.out.println("2022.2.28");
+        System.out.println("2022.2.28");
+        System.out.println("2022.2.28");
+        System.out.println("2022.2.28");
+        System.out.println("2022.2.28");
+        System.out.println("2022.2.29");
+    }
+}
